462908092d3e33d595f14bb654e0c2fe5e904297
Alehandro Persaud <andrew041994@gmail.com>
2019-12-28 00:24:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,7 @@
 def using_push(using_push,next_color)
-  using_push = ["red", "orange", "yellow", "green", "blue", "indigo"]
-  next_color="violet"
   using_push.push(next_color)
+
+end
+def using_unshift ()
 
 end
