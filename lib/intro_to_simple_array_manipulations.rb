1684e323ed111bdf377bf87c2852634b8b7074d0
Alehandro Persaud <andrew041994@gmail.com>
2019-12-28 01:38:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,7 +38,7 @@ def using_flatten(instruments)
   instruments.flatten
 end
 
-def using_delete(instructors)
-  instructors.delete("steven")
+def using_delete(instructors,no_offense_steven)
+  instructors.delete(no_offense_steven)
 
 end
