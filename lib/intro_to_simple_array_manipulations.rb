a8853a275c9d31eb563baf2b59fdcb6dcbb6d8ef
Alehandro Persaud <andrew041994@gmail.com>
2019-12-28 00:32:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,11 @@
 def using_push(using_push,next_color)
   using_push.push(next_color)
-
 end
+
 def using_unshift (bouroughs_in_nyc,new_neighborhood)
 bouroughs_in_nyc.unshift(new_neighborhood)
+end
+
+def using_pop(continents)
+  continents.pop  
 end
