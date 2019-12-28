326329e20392459febf93900e8ad530da46fe05f
Alehandro Persaud <andrew041994@gmail.com>
2019-12-28 01:54:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -43,5 +43,5 @@ def using_delete(instructors,no_offense_steven)
 end
 
 def using_delete_at(famous_robots,using_delete_at)
-  famous_robots.delete(using_delete_at)
+  famous_robots.delete(deleted_robot)
 end
