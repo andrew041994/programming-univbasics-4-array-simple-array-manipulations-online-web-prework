31b3b382c6d9d1cadc769e052eaa868df8e5075a
Alehandro Persaud <andrew041994@gmail.com>
2019-12-28 01:07:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,5 +27,5 @@ def using_concat (my_favorite_things,more_favs)
 end
 
 def using_insert (list_of_programming_languages,new_array)
-  list_of_programming_languages.insert(another_language[5])
+  list_of_programming_languages.insert(a@nother_language[5])
 end
